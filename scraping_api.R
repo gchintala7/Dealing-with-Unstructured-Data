cd646ca76ecaeb2710af3bc758abd9555e630cfe
gchintala7 <147631876+gchintala7@users.noreply.github.com>
2024-02-07 22:35:31 -0500

    Delete scraping_api.R

--- scraping_api.R
+++
@@ -1,7 +0,0 @@
-library(dplyr)
-library(rvest)
-
-link <- read_html("https://www.tdcj.texas.gov/death_row/dr_executed_offenders.html") %>% 
-  html_elements("a[href*='last']") %>%
-  html_attr("href") %>% 
-  grep("last", ., value = TRUE)
